226040523c398ecc5bda2c2b7cf02eaa2145f057
NadaMahmoud44 <45666455+NadaMahmoud44@users.noreply.github.com>
2022-06-28 20:03:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask,render_template, request
+from main import *
+import face_recognition as fr
 
 
 app = Flask(__name__)
@@ -7,13 +9,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def face():
-  if request.method == 'POST':
-   img = request.method['img']
+  if request.method == 'POST':
+    load_image = request.method['img']
+    target_image = fr.load_image_file(load_image)
+    target_encoding = fr.face_encodings(target_image)
+    find_target_face()
 
-   #b = bytes(img, 'utf-8')
-   #print(b)
-   #image= b[b.find(b'/9'):]
-  return render_template('fc.html')
 
-if __name__ == '__main__':
- app.run()
+
+
+   #b = bytes(img, 'utf-8')
+   #print(b)
+   #image= b[b.find(b'/9'):]
+  return render_template('fc.html')
+
+if name == '__main__':
+ app.run()
